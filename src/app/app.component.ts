b6dab7225f34d240c3b6d4ab92b081a66c0a1019
Jeter Medina <42383763+JeterMedina@users.noreply.github.com>
2022-10-07 19:03:56 -0500

    ora si

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {AppRoutingModule} from '../assets/ROUTES/app.routes'
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ecomerce';
+}
